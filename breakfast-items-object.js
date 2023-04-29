735be01cfc82b05e99c6ede211264839079f1c48
moogleKupo <tmaine@gmx.com>
2023-04-29 18:20:40 +0000

    breakfast items

---
+++ breakfast-items-object.js
@@ -0,0 +1,14 @@
+/*
+ * Programming Quiz: Menu Items (7-2)
+ * Create an object named `breakfast`. 
+ * The object should contain properties for the `name`, `price`, and `ingredients`.
+ * Print the entire object on the console
+ */
+
+// your code goes here
+let breakfast = {
+  name: "The Lumberjack",
+  price: "$9.95",
+  ingredients: ["eggs", "toast", "hasbrowns", "pancakes"],
+};
+console.log(breakfast);
